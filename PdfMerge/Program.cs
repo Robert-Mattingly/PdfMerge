bbf61324546d17674c118d08647b96548ff97ba3
Robert Mattingly <Robert-Mattingly@users.noreply.github.com>
2017-11-08 11:13:27 -0500

    Tossing the files up.
    Just getting them clonable for now.

---
+++ PdfMerge/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pdfFiles = inputs.Select(path => PdfReader.Open(path, PdfDocumentOpenMode.Import));
+
+            var outDoc = new PdfDocument();
+
+            foreach (var doc in pdfFiles)
+                outDoc = outDoc.WithPagesFrom(doc);
+
+            outDoc.Save(output);
+        }
+
+
+
+        private static IEnumerable<string> inputs = new string[]
+        {
+            @"D:\Sandbox\Page1.pdf",
+            @"D:\Sandbox\Page2.pdf",
+            @"D:\Sandbox\Page3.pdf",
+            @"D:\Sandbox\Page2.pdf"
+        };
+        private static string output = @"D:\Sandbox\Combined.pdf";
+    }
+
+    public static class PdfExtensions
+    {
+        public static PdfDocument WithPagesFrom(this PdfDocument doc, PdfDocument source)
+        {
+            foreach (PdfPage page in source.Pages)
+                doc.AddPage(page);
+
+            return doc;
+        }
+    }
+}
